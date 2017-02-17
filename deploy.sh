dcc309c41e1d03f371b3db3f14fb4e7552675bb9
Daniel Krook <krook@us.ibm.com>
2017-02-17 12:40:47 -0500

    Update README and refactor

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2016 IBM Corp. All Rights Reserved.
+# Copyright 2017 IBM Corp. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the “License”);
 # you may not use this file except in compliance with the License.
@@ -14,54 +14,43 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Color vars to be used in shell script output
-RED='\033[0;31m'
-YELLOW='\033[0;33m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-
 # Load configuration variables
 source local.env
 
-# Capture the namespace where actions will be created
-WSK='wsk'
-CURRENT_NAMESPACE=`$WSK property get --namespace | sed -n -e 's/^whisk namespace//p' | tr -d '\t '`
-echo "Current namespace is $CURRENT_NAMESPACE."
-
 function usage() {
-  echo -e "${YELLOW}Usage: $0 [--install,--uninstall,--env]${NC}"
+  echo -e "Usage: $0 [--install,--uninstall,--env]"
 }
 
 function install() {
-  echo -e "${YELLOW}Installing OpenWhisk actions, triggers, and rules for check-deposit..."
+  echo -e "Installing OpenWhisk actions, triggers, and rules for openwhisk-serverless-apis..."
 
-  echo "Binding package"
+  echo "Binding Cloudant package"
   wsk package bind /whisk.system/cloudant "$CLOUDANT_INSTANCE" \
-  --param username "$CLOUDANT_USER" \
-  --param password "$CLOUDANT_PASS" \
-  --param host "$CLOUDANT_USER.cloudant.com" \
-  --param dbname "$CLOUDANT_DATABASE"
+    --param username "$CLOUDANT_USERNAME" \
+    --param password "$CLOUDANT_PASSWORD" \
+    --param host "$CLOUDANT_USERNAME.cloudant.com" \
+    --param dbname "$CLOUDANT_DATABASE"
 
   echo "Creating actions"
-  wsk action create transformDataForWrite actions/transformDataForWrite.js 
+  wsk action create transformDataForWrite actions/transformDataForWrite.js
 
   wsk action create --sequence createDocumentSequence \
-    transformDataForWrite,/$CURRENT_NAMESPACE/$CLOUDANT_INSTANCE/create-document
+    transformDataForWrite,/_/$CLOUDANT_INSTANCE/create-document
+
   wsk action create --sequence updateDocumentSequence \
-    transformDataForWrite,/$CURRENT_NAMESPACE/$CLOUDANT_INSTANCE/update-document
+    transformDataForWrite,/_/$CLOUDANT_INSTANCE/update-document
 
-  echo "Creating API actions"
-  wsk api-experimental create -n "Cats API" /example /cats get /$CURRENT_NAMESPACE/$CLOUDANT_INSTANCE/read-document
-  wsk api-experimental create /example /cats delete /$CURRENT_NAMESPACE/$CLOUDANT_INSTANCE/delete-document
-
+  echo "Creating REST API actions"
+  wsk api-experimental create -n "Cats API" /example /cats get /_/$CLOUDANT_INSTANCE/read-document
+  wsk api-experimental create /example /cats delete /_/$CLOUDANT_INSTANCE/delete-document
   wsk api-experimental create /example /cats put updateDocumentSequence
   wsk api-experimental create /example /cats post createDocumentSequence
 
-  echo -e "${GREEN}Install Complete${NC}"
+  echo -e "Install Complete"
 }
 
 function uninstall() {
-  echo -e "${RED}Uninstalling..."
+  echo -e "Uninstalling..."
 
   echo "Removing API actions..."
   wsk api-experimental delete /example
@@ -72,14 +61,16 @@ function uninstall() {
   wsk action delete transformDataForWrite
 
   echo "Removing packages..."
-  $WSK package delete "$CLOUDANT_INSTANCE"
+  wsk package delete "$CLOUDANT_INSTANCE"
 
-  echo -e "${GREEN}Uninstall Complete${NC}"
+  echo -e "Uninstall Complete"
 }
 
 function showenv() {
-  echo -e "${YELLOW}"
-  echo -e "${NC}"
+  echo -e "$CLOUDANT_INSTANCE"
+  echo -e "$CLOUDANT_USERNAME"
+  echo -e "$CLOUDANT_PASSWORD"
+  echo -e "$CLOUDANT_DATABASE"
 }
 
 case "$1" in
